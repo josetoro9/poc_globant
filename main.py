521e69b2190e9f6a5b3240f2d31452b51889c407
Jose Antonio Toro Ospina <94059096+josetoro9@users.noreply.github.com>
2023-04-10 17:15:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,23 +7,14 @@ Created on Mon Apr 10 09:47:52 2023
 import pandas as pd
 import os
 import logging
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Table
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import text
+from sqlalchemy.ext.declarative import declarative_base
 import glob
-import pip
-import pkg_resources
-from flask import Flask, request
 import csv
 
 
-#To install
 
-required = {'pymysql'}
-installed = {pkg.key for pkg in pkg_resources.working_set}
-missing = required - installed
-if missing:
-    pip.main(['install']+list(missing))
 
 
 def title(csv_file):
@@ -45,7 +36,6 @@ def mysql_data(df,name,required_fields):
     df_mysql = df.applymap(lambda x: f'"{x}"' if type(x) == str and '"' in x else x)
     data = df_mysql.to_csv(index=False, header=False, quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
 
-
     # Escribe los datos transformados en un archivo CSV
     with open(name +'_mysql.csv', 'w') as file:
         file.write(cadena+'\n') # nombres de las columnas
@@ -53,6 +43,16 @@ def mysql_data(df,name,required_fields):
 
 path = os.path.dirname(os.path.abspath(__file__))
 os.chdir(path)
+
+# Configure logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s %(message)s',
+    handlers=[
+        logging.FileHandler('myapp.log', mode='a'),
+        logging.StreamHandler()
+    ]
+)
 
 # Define the data dictionary rules for the tables
 data_dict = {
@@ -81,22 +81,33 @@ for name in filtered_files:
         departments = validate_data(name, required_fields)
         mysql_data(departments,title(name),required_fields)
         
+
+# Define batch size
+BATCH_SIZE = 1000
+
+
 #Loading data to mysql
 for name in glob.glob(path + '/*_mysql.csv'):
-    engine = create_engine(DATABASE_URL)
     table_name = title(name)[:-6]
-    table = pd.read_csv(name)
-    # in this case we can change the if exists parameter eg: 'replace','append'
-    table.to_sql(table_name, engine, if_exists='replace', index=False)
-    #To close sql alchemy connection
-    engine.dispose()
-    print(table_name)
-    
-print(bbb)
+    try:
+        for chunk in pd.read_csv(name, chunksize=BATCH_SIZE):
+            engine = create_engine(DATABASE_URL)
+            # in this case we can change the if exists parameter eg: 'replace','append'
+            chunk.to_sql(table_name, engine, if_exists='append', index=False)
+            #To close sql alchemy connection
+            engine.dispose()
+    except:
+        logging.error("Failed to load data for table {}". format(table_name))
+
+
+logging.debug('This is a debug message')
+logging.info('This is an info message')
+logging.warning('This is a warning message')
+logging.error('This is an error message')
+logging.critical('This is a critical message')    
+
+logging.shutdown()
 
 
 
-
-
-
-
+ 

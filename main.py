723bcce843007d40c1a40f74a413f7a8447d88c5
Jose Antonio Toro Ospina <94059096+josetoro9@users.noreply.github.com>
2023-04-10 22:52:24 -0500

    avro files
    Setting up Avro files for backups, and performing data cleaning in the path to avoid saving unnecessary data.

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import logging
 from sqlalchemy import create_engine
 import glob
 import csv
+import fastavro
+from fastavro import writer, parse_schema
 
 def inic_logger(path, namelog):
     global logger
@@ -19,7 +21,7 @@ def inic_logger(path, namelog):
     if not os.path.exists(directory):
         os.makedirs(directory)
     
-    # Verificar si el controlador ya está en la lista
+    # Verifying if the handler is already in the list
     if not any(isinstance(h, logging.FileHandler) and h.baseFilename == directory + namelog + '.log' for h in logger.handlers):
         hdlr = logging.FileHandler(directory + namelog + '.log')
         formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] [%(filename)s] :%(message)s')
@@ -47,19 +49,21 @@ def mysql_data(df,name,required_fields,situation = None):
     else:
         text = '_backup.csv'
     cadena = ', '.join(required_fields)
-    # Transforma los datos para el formato MySQL
+    # Transforming data for MySQL format
     df_mysql = df.applymap(lambda x: f'"{x}"' if type(x) == str and '"' in x else x)
-    data = df_mysql.to_csv(index=False, header=False, quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
-    # Escribe los datos transformados en un archivo CSV
+    data = df_mysql.to_csv(index=False, header=False, quotechar='"', quoting=csv.QUOTE_NONNUMERIC, encoding='utf-8')
+    # Writing the transformed data to a CSV file
     with open(name + text, 'w') as file:
-        file.write(cadena+'\n') # nombres de las columnas
+        file.write(cadena+'\n') # Column names
         file.write(data)
 
 path = os.path.dirname(os.path.abspath(__file__))
 os.chdir(path)
 namelog = 'poc_globant'
 
-execute_as_buckUp = False
+execute_as_buckUp = True
+# in case there are files that you dont want to reload from the buckup
+archivos_a_eliminar = ['hired_employees', 'jobs'] 
 
 inic_logger(path, namelog)
 # Define the data dictionary rules for the tables
@@ -72,6 +76,24 @@ DATABASE_URL = 'mysql+pymysql://root:passcode@127.0.0.1/mydatabase'
 files = glob.glob(path + '/*.csv')
 # Define batch size
 BATCH_SIZE = 1000
+
+
+#we have to use this dict beacause avro does not support int64 dtype
+type_mapping = {
+    "integer":  "int",
+    "floating":  "double",
+    "boolean":  "boolean",
+    "string":  "string",
+    "mixed-integer":  "int",
+    "mixed-integer-float":  "double",
+    "decimal":  "double",
+    "complex":  "string",
+    "categorical":  "string",
+    "datetime":  "string",
+    "timedelta":  "string",
+    "mixed":  "string",
+    "empty":  "null"
+}
 
 if execute_as_buckUp == False:
     logger.info("Normal execution (no backup)")
@@ -113,29 +135,53 @@ if execute_as_buckUp == False:
         engine = create_engine(DATABASE_URL)
         query = f"select * FROM {title(name)};"
         df = pd.read_sql(query, engine)
-        mysql_data(df,title(name),required_fields,situation = 'backup')
+        column_types = {column: pd.api.types.infer_dtype(df[column]) for column in df.columns}
+        avro_schema = {
+            "type": "record",
+            "name": title(name),
+            "fields": [
+                {"name": column, "type": ["null", type_mapping[column_types[column]]]} 
+                for column in df.columns
+                ]
+            }
+        records = df.to_dict(orient='records')
+        with open(title(name) +'.avro', 'wb') as out_file:
+            writer(out_file, parse_schema(avro_schema), records)
+        #mysql_data(df,title(name),required_fields,situation = 'backup')
         
 else:
     logger.info("BACKUP execution")
-    filtered_files = [file for file in files if '_backup'  in file] 
-    for name in filtered_files:
+    avrofiles = glob.glob(path + '/*.avro')
+    # Eliminar elementos de la lista original
+    for archivo in archivos_a_eliminar:
+        avrofiles.remove(os.path.join(path, archivo + '.avro'))
+    for name in avrofiles:
         engine = create_engine(DATABASE_URL)
-        table_name = title(name)[:-7]
+        table_name = title(name)[:-1]
         #We delete the tables before loading the backup.
         query = f"drop table if exists {table_name};"
         engine.execute(query)
         required_fields = data_dict[table_name]
         cadena = ', '.join(required_fields)
+        # Abre el archivo Avro y crea el objeto Reader
+        with open(table_name + '.avro', 'rb') as avro_file:
+            reader = fastavro.reader(avro_file)
+            # Itera sobre los datos recuperados
+            rows = []
+            for record in reader:
+                rows.append(record)
+        dfavro = pd.DataFrame(rows)
+        mysql_data(dfavro,table_name,required_fields)
         try:
-            for chunk in pd.read_csv(name, chunksize=BATCH_SIZE):
+            for chunk in pd.read_csv(name[:-5]+'_mysql.csv', chunksize=BATCH_SIZE):
                 chunk.to_sql(table_name, engine, if_exists='append', index=False)
-                
         except:
             logger.error("Failed to load data for table {}". format(table_name))
         #To close sql alchemy connection
         engine.dispose()
+        
+for archivo in os.listdir(path):
+    if archivo.endswith('_mysql.csv'):
+        os.remove(os.path.join(path, archivo))
 
 logging.shutdown()
-
-
-    
